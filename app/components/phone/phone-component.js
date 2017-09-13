ff7a86afe3450f8d61ab0595d25def4513af82f9
Tiago Matana <tiago.matana@gmail.com>
2017-09-13 18:15:05 -0300

    BOTTOM SHEET

--- app/components/phone/phone-component.js
+++ app/components/phone/phone-component.js
@@ -2,11 +2,32 @@
   angular
   .module('InovaApp')
   .component('phone',{
-    templateUrl: 'app/components/phone/phone-template.html',
+    templateUrl: 'app/components/phone/contatos.html',
     controller: Controller
   });
+  Controller.$inject = [
+    '$scope',
+    '$mdBottomSheet'
+  ]
 
-  function Controller() {
-
+  function Controller($scope, $mdBottomSheet) {
+    var self = this
+    self.showGridBottomSheet = function() {
+    self.alert = '';
+    $mdBottomSheet.show({
+      templateUrl: 'app/components/phone/phone-template.html',
+      controller: self.controller,
+      clickOutsideToClose: false
+    }).then(function(clickedItem) {
+      $mdToast.show(
+            $mdToast.simple()
+              .textContent(clickedItem['name'] + ' clicked!')
+              .position('top right')
+              .hideDelay(1500)
+          );
+    }).catch(function(error) {
+      // User clicked outside or hit escape
+    });
+  };
   }
 }());
