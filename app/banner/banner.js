923e08e48052942da41c8f52467b8f9a8cbd848b
Tiago Matana <tiago.matana@gmail.com>
2017-09-13 14:08:06 -0300

    Sidenav

--- app/banner/banner.js
+++ app/banner/banner.js
@@ -1,7 +1,7 @@
 (function() {
   angular.module('InovaApp', ['ngMaterial', 'jkAngularCarousel'])
 
-    .controller('MyCtrl', function($scope) {
+    .controller('MyCtrl', function($scope, $timeout, $mdSidenav, $log) {
       $scope.dataArray = [{
           src: 'assets/images/1.jpg',
           label : 'Energia Solar Fotovoltaica- Orçamento sem compromisso.'
@@ -19,5 +19,32 @@
           label: 'Correção Fator de Potência.'
         }
       ];
+
+      $scope.toggleMenu = buildToggler('left');
+
+      function debounce(func, wait, context) {
+        var timer;
+
+        return function debounced() {
+          var context = $scope,
+              args = Array.prototype.slice.call(arguments);
+          $timeout.cancel(timer);
+          timer = $timeout(function() {
+            timer = undefined;
+            func.apply(context, args);
+          }, wait || 10);
+        };
+      }
+
+      function buildToggler(navID) {
+        return function() {
+          // Component lookup should always be available since we are not using `ng-if`
+          $mdSidenav(navID)
+            .toggle()
+            .then(function () {
+              $log.debug("toggle " + navID + " is done");
+            });
+        };
+      }
     });
 }());
--- app/index.js
+++ app/index.js
@@ -1,4 +1,31 @@
 (function() {
-  angular
-  .module('InovaApp', ['ngMaterial']);
+  angular.module('InovaApp', ['ngMaterial'])
+  .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.toggleMenu = buildToggler('left');
+
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    function buildToggler(navID) {
+      return function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      };
+    }
+  });
 }());
