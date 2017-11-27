0b18c226ca1d3af1d1c317292e2b84d02d60e1c5
Tiago Matana <tiago.matana@gmail.com>
2017-11-27 20:48:25 -0200

    opacity 7

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 				<!-- Main -->
 					<section id="main">
 						<header>
-							<span><img src="images/logocompleto.png" width="160" alt="" /></span>
+							<span><img src="images/logocompleto.png" width="160" alt="" style="opacity:0.7"/></span>
 							<!-- <h1>INOVA ENERGIA SOLAR</h1>
 							<p>Senior Psychonautics Engineer</p> -->
 							<!-- <?php
